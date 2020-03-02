1ce83740e5a44e97fbaac1b5342f0beeb25de669
heryfidiawan07 <heryfidiawan07@gmail.com>
2020-03-02 23:05:07 +0700

    add carousel home page

--- resources/views/guest/home.blade.php
+++ resources/views/guest/home.blade.php
@@ -7,7 +7,182 @@ Home
 @section('content')
 <section class="section">
 	<div class="section-body">
-		<h1>HOME</h1>
+
+		<div class="row">
+
+			<div class="col-md-6 p-1">
+				<div id="carouselExampleIndicators2" class="carousel slide" data-ride="carousel">
+					<ol class="carousel-indicators">
+						<li data-target="#carouselExampleIndicators2" data-slide-to="0" class=""></li>
+						<li data-target="#carouselExampleIndicators2" data-slide-to="1" class="active"></li>
+						<li data-target="#carouselExampleIndicators2" data-slide-to="2" class=""></li>
+					</ol>
+					<div class="carousel-inner">
+						<div class="carousel-item">
+							<img class="d-block w-100" src="https://picsum.photos/id/0/5616/3744" alt="First slide">
+							<div class="carousel-caption d-none d-md-block">
+								<h5>Heading</h5>
+								<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+								tempor incididunt ut labore et dolore magna aliqua.</p>
+							</div>
+						</div>
+						<div class="carousel-item active">
+							<img class="d-block w-100" src="https://picsum.photos/id/1001/5616/3744" alt="Second slide">
+							<div class="carousel-caption d-none d-md-block">
+								<h5>Heading</h5>
+								<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+								tempor incididunt ut labore et dolore magna aliqua.</p>
+							</div>
+						</div>
+						<div class="carousel-item">
+							<img class="d-block w-100" src="https://picsum.photos/id/1005/5760/3840" alt="Third slide">
+							<div class="carousel-caption d-none d-md-block">
+								<h5>Heading</h5>
+								<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+								tempor incididunt ut labore et dolore magna aliqua.</p>
+							</div>
+						</div>
+					</div>
+					<a class="carousel-control-prev" href="#carouselExampleIndicators2" role="button" data-slide="prev">
+						<span class="carousel-control-prev-icon" aria-hidden="true"></span>
+						<span class="sr-only">Previous</span>
+					</a>
+					<a class="carousel-control-next" href="#carouselExampleIndicators2" role="button" data-slide="next">
+						<span class="carousel-control-next-icon" aria-hidden="true"></span>
+						<span class="sr-only">Next</span>
+					</a>
+				</div>
+			</div>
+
+			<div class="col-md-6">
+				<div class="row">
+					<div class="col-md-6 p-1">
+						<div id="carouselExampleIndicators2" class="carousel slide" data-ride="carousel">
+							<div class="carousel-inner">
+								<div class="carousel-item">
+									<img class="d-block w-100" src="https://picsum.photos/id/0/5616/3744" alt="First slide">
+									<div class="carousel-caption d-none d-md-block">
+										<h5>Heading</h5>
+										<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+										tempor incididunt ut labore et dolore magna aliqua.</p>
+									</div>
+								</div>
+								<div class="carousel-item active">
+									<img class="d-block w-100" src="https://picsum.photos/id/1001/5616/3744" alt="Second slide">
+									<div class="carousel-caption d-none d-md-block">
+										<h5>Heading</h5>
+										<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+										tempor incididunt ut labore et dolore magna aliqua.</p>
+									</div>
+								</div>
+								<div class="carousel-item">
+									<img class="d-block w-100" src="https://picsum.photos/id/1005/5760/3840" alt="Third slide">
+									<div class="carousel-caption d-none d-md-block">
+										<h5>Heading</h5>
+										<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+										tempor incididunt ut labore et dolore magna aliqua.</p>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+					<div class="col-md-6 p-1">
+						<div id="carouselExampleIndicators2" class="carousel slide" data-ride="carousel">
+							<div class="carousel-inner">
+								<div class="carousel-item">
+									<img class="d-block w-100" src="https://picsum.photos/id/0/5616/3744" alt="First slide">
+									<div class="carousel-caption d-none d-md-block">
+										<h5>Heading</h5>
+										<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+										tempor incididunt ut labore et dolore magna aliqua.</p>
+									</div>
+								</div>
+								<div class="carousel-item active">
+									<img class="d-block w-100" src="https://picsum.photos/id/1001/5616/3744" alt="Second slide">
+									<div class="carousel-caption d-none d-md-block">
+										<h5>Heading</h5>
+										<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+										tempor incididunt ut labore et dolore magna aliqua.</p>
+									</div>
+								</div>
+								<div class="carousel-item">
+									<img class="d-block w-100" src="https://picsum.photos/id/1005/5760/3840" alt="Third slide">
+									<div class="carousel-caption d-none d-md-block">
+										<h5>Heading</h5>
+										<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+										tempor incididunt ut labore et dolore magna aliqua.</p>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+				<div class="row">
+					<div class="col-md-6 p-1">
+						<div id="carouselExampleIndicators2" class="carousel slide" data-ride="carousel">
+							<div class="carousel-inner">
+								<div class="carousel-item">
+									<img class="d-block w-100" src="https://picsum.photos/id/0/5616/3744" alt="First slide">
+									<div class="carousel-caption d-none d-md-block">
+										<h5>Heading</h5>
+										<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+										tempor incididunt ut labore et dolore magna aliqua.</p>
+									</div>
+								</div>
+								<div class="carousel-item active">
+									<img class="d-block w-100" src="https://picsum.photos/id/1001/5616/3744" alt="Second slide">
+									<div class="carousel-caption d-none d-md-block">
+										<h5>Heading</h5>
+										<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+										tempor incididunt ut labore et dolore magna aliqua.</p>
+									</div>
+								</div>
+								<div class="carousel-item">
+									<img class="d-block w-100" src="https://picsum.photos/id/1005/5760/3840" alt="Third slide">
+									<div class="carousel-caption d-none d-md-block">
+										<h5>Heading</h5>
+										<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+										tempor incididunt ut labore et dolore magna aliqua.</p>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+					<div class="col-md-6 p-1">
+						<div id="carouselExampleIndicators2" class="carousel slide" data-ride="carousel">
+							<div class="carousel-inner">
+								<div class="carousel-item">
+									<img class="d-block w-100" src="https://picsum.photos/id/0/5616/3744" alt="First slide">
+									<div class="carousel-caption d-none d-md-block">
+										<h5>Heading</h5>
+										<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+										tempor incididunt ut labore et dolore magna aliqua.</p>
+									</div>
+								</div>
+								<div class="carousel-item active">
+									<img class="d-block w-100" src="https://picsum.photos/id/1001/5616/3744" alt="Second slide">
+									<div class="carousel-caption d-none d-md-block">
+										<h5>Heading</h5>
+										<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+										tempor incididunt ut labore et dolore magna aliqua.</p>
+									</div>
+								</div>
+								<div class="carousel-item">
+									<img class="d-block w-100" src="https://picsum.photos/id/1005/5760/3840" alt="Third slide">
+									<div class="carousel-caption d-none d-md-block">
+										<h5>Heading</h5>
+										<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+										tempor incididunt ut labore et dolore magna aliqua.</p>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+			
+		</div>
+		
 	</div>
 </section>
 @endsection
