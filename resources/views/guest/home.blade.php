ea8312b23428a4620f8271c8a4b2b85d7fd92570
heryfidiawan07 <heryfidiawan07@gmail.com>
2020-03-03 22:56:30 +0700

    add all page

--- resources/views/guest/home.blade.php
+++ resources/views/guest/home.blade.php
@@ -9,7 +9,6 @@ Home
 	<div class="section-body">
 
 		<div class="row">
-
 			<div class="col-md-6 p-1">
 				<div id="carouselExampleIndicators2" class="carousel slide" data-ride="carousel">
 					<ol class="carousel-indicators">
@@ -179,8 +178,26 @@ Home
 						</div>
 					</div>
 				</div>
+			</div>			
+		</div>
+
+		<div class="row mt-3">
+			<div class="col-md-8 p-1">
+				<h2 class="section-title">Functionality Card</h2>
+				@for($i=0; $i<5; $i++)
+					<div class="card card-primary mb-2">
+						<div class="card-body">
+							<a href="#" class="text-primary">
+								<h5>The title lorem ipsum dolor sit amet</h5>
+							</a>
+							<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Soluta architecto ipsa corrupti, sapiente possimus esse, corporis eius facere optio quasi facilis aliquid laudantium voluptatum debitis fugiat molestias. Velit, aut est?</p>
+							<span><i class="far fa-heart"></i> 100</span>
+							<span class="ml-3"><i class="far fa-comment"></i> 100</span>
+						</div>
+					</div>
+				@endfor
 			</div>
-			
+			<div class="col-md-4"></div>
 		</div>
 		
 	</div>
--- resources/views/guest/show.blade.php
+++ resources/views/guest/show.blade.php
@@ -6,8 +6,65 @@ SHOW
 
 @section('content')
 <section class="section">
-	<div class="section-body">
-		<h1>SHOW</h1>
-	</div>
+<div class="section-body">
+
+    <div class="row">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header">
+                    <h4>Example Card</h4>
+                </div>
+                <div class="card-body">
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+                        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+                        quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+                        consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+                        cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+                    proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                    <span><i class="far fa-heart"></i> 100</span>
+                    <span class="ml-3"><i class="far fa-comment"></i> 100</span>
+                    <hr>
+                    <div class="media mb-2">
+                        <img class="mr-3 rounded-circle" src="assets/img/example-image-50.jpg" alt="Generic placeholder image">
+                        <div class="media-body">
+                            <h6 class="mt-0">Media heading</h6>
+                            <p>Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin. Cras purus odio, vestibulum in vulputate at.</p>
+                            <span><i class="far fa-heart"></i> 100</span>
+                            <span class="ml-3"><i class="far fa-comment"></i> 100</span>
+                            <div class="media mt-3">
+                                <a class="pr-3" href="#">
+                                    <img class="rounded-circle" src="assets/img/example-image-50.jpg" alt="Generic placeholder image">
+                                </a>
+                                <div class="media-body">
+                                    <h6 class="mt-0">Media heading</h6>
+                                    <p class="mb-0">Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin. Cras purus odio.</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="media mb-2">
+                        <img class="mr-3 rounded-circle" src="assets/img/example-image-50.jpg" alt="Generic placeholder image">
+                        <div class="media-body">
+                            <h6 class="mt-0">Media heading</h6>
+                            <p>Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin. Cras purus odio, vestibulum in vulputate at.</p>
+                            <span><i class="far fa-heart"></i> 100</span>
+                            <span class="ml-3"><i class="far fa-comment"></i> 100</span>
+                            <div class="media mt-3">
+                                <a class="pr-3" href="#">
+                                    <img class="rounded-circle" src="assets/img/example-image-50.jpg" alt="Generic placeholder image">
+                                </a>
+                                <div class="media-body">
+                                    <h6 class="mt-0">Media heading</h6>
+                                    <p class="mb-0">Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin. Cras purus odio.</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+</div>
 </section>
 @endsection
